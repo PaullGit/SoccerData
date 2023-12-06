6d6de768f38afc55c6a3900555cc0f355f9b793d
PaullGit <paultolnor1@outlook.com>
2023-12-06 14:54:48 +0100

    Top 5 leagues added

--- functions.py
+++ functions.py
@@ -1,31 +1,28 @@
 
-def GetPlayerName(club_id, pageSoup):
+def GetPlayers(club_id, pageSoup):
     html_names = pageSoup.find_all("img", {"class": "bilderrahmen-fixed lazy lazy"})
     list_names = [element['alt'] for element in html_names] 
-    return list_names
 
-def GetPlayerID(club_id, pageSoup):
-    print(club_id)  
     html_ids = pageSoup.find_all("td", {"class": "rechts hauptlink"})
     list_ids = []
     
     for element in html_ids:
         html_id= str(element)
         parts = html_id.split("/")
-        if len(parts) < 5:
-            continue   
-        player_id = parts[4].split(">")[0]
-        cleaned_player_id = ''.join(char for char in player_id if char.isdigit())
+        if len(parts) > 5:   
+            player_id = parts[4].split(">")[0]
+            cleaned_player_id = ''.join(char for char in player_id if char.isdigit())
+        else: 
+            player_id = "NULL"
         list_ids.append(cleaned_player_id)
     list_club_ids = [club_id for i in range(0, len(list_ids))]
-    return list_ids, list_club_ids
+    Validate (list_names, list_ids, club_id)
+    return list_ids,list_names, list_club_ids
 
 
-def Validate(a ,b ):
-    if len(a) == len(b):
-        return True
-    else:
+def Validate(a ,b, club_id):
+    if len(a) != len(b):
         print("a: " + str( len(a)) )
         print("b: " + str( len(b)) )
-        return False
+        print("False on club_id: " + str(club_id))
         
--- program.py
+++ program.py
@@ -11,20 +11,23 @@ ClubIDs = []
 premier_league = [985, 11, 281,631,31,148, 762, 405, 379, 1237, 703, 1148, 873, 29, 989, 543, 931,1132, 350,1031]
 series_a = [6195, 46, 5, 506, 12, 800, 398, 430,416, 1025, 6574, 410, 252, 2919, 380, 749, 276, 1005, 1390, 8970]
 eredivisie = [234, 383, 610,1090,317,200,306,467,468,499,798,385,1435,1269,723,1304,724,235]
-ligue_1 = []
-la_liga = []
-bundesliga = []
+ligue_1 = [583, 162, 273, 244, 417, 1082, 826, 1041,667, 995, 1421, 1158, 969,415, 3911,738, 3524, 347]
+la_liga = [418, 131, 13, 681, 621,150, 368, 1050, 1049, 12321, 940,331,3709,3302, 237, 367, 16795, 2687, 472,1108]
+bundesliga = [27, 15, 23826, 16, 82, 18, 89, 24, 79, 60, 533, 167, 39, 3, 86, 80, 2036, 105]
+brasileiro = [614,1023,585,679,199,300, 2462, 6600, 221, 8793, 978,537, 10010, 210, 609, 776, 10870, 2863, 3197, 28022]
 
+leagues = [premier_league, series_a, eredivisie, ligue_1, la_liga, bundesliga]
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux ×86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
-for club_id in eredivisie:
-    page =  f"https://www.transfermarkt.co.uk/clubname/startseite/verein/{club_id}"
-    pageTree = requests.get(page, headers=headers)
-    pageSoup = BeautifulSoup(pageTree. content, 'html.parser')
-    Names += f.GetPlayerName(club_id, pageSoup)
-    IDs += f.GetPlayerID(club_id, pageSoup)[0]
-    ClubIDs += f.GetPlayerID(club_id, pageSoup)[1]
-    print(str (f.Validate(Names, IDs)) + " " + str(club_id))
-
+for league in leagues:
+    for club_id in league:
+        page =  f"https://www.transfermarkt.co.uk/clubname/startseite/verein/{club_id}"
+        pageTree = requests.get(page, headers=headers)
+        pageSoup = BeautifulSoup(pageTree. content, 'html.parser')
+        name_id_club = f.GetPlayers(club_id, pageSoup)
+        IDs += name_id_club[0]
+        Names += name_id_club[1]
+        ClubIDs += name_id_club[2]
 
 df = pd.DataFrame({"IDs": IDs, "Names": Names, "ClubIDs": ClubIDs })
+
 print(df)
