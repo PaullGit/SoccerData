608d7e88570b6d2682a24c8c9424c75d1a49abf1
PaullGit <94172003+PaullGit@users.noreply.github.com>
2024-01-11 15:16:13 +0100

    Jpt Notebook test_player_porfile doet het!
    Alleen de py versie doet het helaas nog niet helemaal!

--- functions.py
+++ functions.py
@@ -90,72 +90,38 @@ def GetPlayerProfile(player, pageSoup):
     current_market_value_meta = pageSoup.find_all("div", {"class":"current-value svelte-18lvpom"}) 
     #SEEDING DATA
     player_id_list = []
+    player_club_list= []
     date_of_birth_list = []
     place_of_birth_list = []
     citizenship_list = []
     height_list = []
-
-    name_in_country_list = []
-    position_list = []
-    foot_list = []
-
-    current_club_list = []
     current_market_value_list = []
     
     player_id_list.append(player)
-    current_club_list.append(current_club_meta[0].text.strip())
-    current_market_value_list.append("NULL")
-    if hasCountryName(player_data[0]):
-        for _ in range(0, 7):
-                html_string = player_data[_]
-                soup = BeautifulSoup(str(html_string), 'html.parser')
-                td_tag = soup.find("span", class_="info-table__content info-table__content--bold")   
 
-                if td_tag:
-                    if _ % 8 == 0:  
-                        name_in_country_list.append(td_tag.text.strip())
-                    if _ % 8 == 1:
-                        date_of_birth_list.append(td_tag.text.strip())
-                    if _ % 8 == 2:
-                        place_of_birth_list.append(td_tag.text.strip())
-                    if _ % 8 == 3:
-                        height_list.append(td_tag.text.strip()) 
-                    if _ % 8 == 4:
-                        citizenship_list.append(td_tag.text.strip())
-                    if _ % 8 == 5:
-                        position_list.append(td_tag.text.strip())
-                    if _ % 8 == 6:  
-                        foot_list.append(td_tag.text.strip())
-                else:
-                    print("NULL")
-    else:
-        for _ in range(0, 6):
-                html_string = player_data[_]
-                soup = BeautifulSoup(str(html_string), 'html.parser')
-                td_tag = soup.find("span", class_="info-table__content info-table__content--bold")   
+    club_soup = BeautifulSoup(str(current_club_meta[0]), 'html.parser')
+    a_tag = club_soup.find('span', class_="data-header__club").find('a')
+    club_name = a_tag.get('title')
+    player_club_list.append(club_name)
 
-                if td_tag:
-                    if _ % 7 == 0:  
-                        date_of_birth_list.append(td_tag.text.strip())
-                    if _ % 7 == 1:
-                        place_of_birth_list.append(td_tag.text.strip())
-                    if _ % 7 == 2:
-                        height_list.append(td_tag.text.strip()) 
-                    if _ % 7 == 3:
-                        citizenship_list.append(td_tag.text.strip())
-                    if _ % 7 == 4:
-                        position_list.append(td_tag.text.strip())
-                    if _ % 7 == 5:  
-                        foot_list.append(td_tag.text.strip())
-                else:
-                    print("NULL")
-        return player_id_list, name_in_country_list, date_of_birth_list, place_of_birth_list, height_list, citizenship_list, foot_list, current_club_list, current_market_value_list
+    current_market_value_list.append("NULL") # Has to be changed!
 
-def hasCountryName(first):
-    soup = BeautifulSoup(str(first), 'html.parser')
-    td_tag = soup.find("span", class_="info-table__content info-table__content--bold")
+    for _ in range(0, len(player_data)):
+        html_string = player_data[_]
+        soup = BeautifulSoup(str(html_string), 'html.parser')
+        td_tag = soup.find("span", class_="data-header__content")
+        print(td_tag)
+        if td_tag:
+            if _ % 8 == 0:  
+                date_of_birth_list.append(td_tag.text.strip())
+            if _ % 8 == 1:
+                place_of_birth_list.append(td_tag.text.strip())
+            if _ % 8 == 2:
+                citizenship_list.append(td_tag.text.strip())
+            if _ % 8 == 3:
+                height_list.append(td_tag.text.strip())
+            
+        else:
+            print("NULL")
+    return player_id_list, player_club_list, date_of_birth_list, place_of_birth_list, height_list, citizenship_list, current_market_value_list
 
-    if td_tag.text.strip() == "Name in home country:":
-        return False
-    else:
-    
--- player_profile.py
+++ player_profile.py
@@ -16,18 +16,16 @@ stopwatch.start()
 # Takes a CSV from all players
 read = pd.read_csv("all_players.csv")
 # players = read["PlayerID"].dropna().tolist()
-players = [182877,335721,230784,392765,776890,282823,357164]
-
+# players = [182877,335721,230784,392765,776890,282823,357164]
+players = [418560,418560]
 player_id_list = []
-name_in_country_list = []
+player_club_list= []
 date_of_birth_list = []
 place_of_birth_list = []
-height_list = []
 citizenship_list = []
-position_list = []
-foot_list = []
-current_club_list = []
-current_market_value_list = []
+height_list = []
+
+current_market_value_list = [] #TBD
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux ×86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 for player in players:
@@ -37,17 +35,15 @@ for player in players:
     result = f.GetPlayerProfile(int(player), pageSoup)
 
     player_id_list += result[0]
-    name_in_country_list += result[1]
+    player_club_list += result[1]
     date_of_birth_list += result[2]
     place_of_birth_list += result[3]
     height_list += result[4]
     citizenship_list+= result[5]
-    foot_list += result[6]
-    current_club_list += result[7]
-    current_market_value_list += result[8] # Has to be added in Functions.py
+    current_market_value_list += result[6] # Has to be added in Functions.py
     
-df = pd.DataFrame({"PlayerID": player_id_list, "Name in Country": name_in_country_list, "Date of Birth": date_of_birth_list, "Place of Birth": place_of_birth_list, "Height": height_list, "Citizenship": citizenship_list, "Foot": foot_list, "Current Club": current_club_list, "Current Market Value": current_market_value_list})
-df.to_csv("player_profile.csv")  
+df = pd.DataFrame({"PlayerID":player_id_list, "Club":player_club_list, "DateOfBirth":date_of_birth_list, "PlaceOfBirth":place_of_birth_list, "Height":height_list, "Citizenship":citizenship_list, "CurrentMarketValue":current_market_value_list})
+# df.to_csv("player_profile.csv") 
 print(df)
 
 elapsed_time = stopwatch.stop()
